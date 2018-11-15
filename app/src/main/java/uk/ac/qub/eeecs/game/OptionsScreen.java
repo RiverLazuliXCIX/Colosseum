09860a32632452db3cadb430c147f82cd41db6a3
Scott Barham <sbarham01@qub.ac.uk>
2018-11-15 15:24:37 +0000

    User Story O3: Providing a background for the options screen and Story O6: Returning to the main screen from the options screen

--- app/src/main/java/uk/ac/qub/eeecs/game/OptionsScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/OptionsScreen.java
@@ -2,14 +2,23 @@ package uk.ac.qub.eeecs.game;
 
 import android.graphics.Color;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uk.ac.qub.eeecs.gage.Game;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
 import uk.ac.qub.eeecs.gage.engine.input.Input;
+import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
+import uk.ac.qub.eeecs.gage.ui.PushButton;
+import uk.ac.qub.eeecs.gage.world.GameObject;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
 
 public class OptionsScreen extends GameScreen {
 
+    private PushButton mBackButton;
+    private List<PushButton> mButtons = new ArrayList<>();
+    private GameObject mOptionBackground;
     // /////////////////////////////////////////////////////////////////////////
     // Constructors
     // /////////////////////////////////////////////////////////////////////////
@@ -22,6 +31,21 @@ public class OptionsScreen extends GameScreen {
 
     public OptionsScreen(Game game) {
         super("OptionScreen", game);
+
+        // Load in the bitmap used for the back button, this could later be moved to a seperate "optionscreenassets.json" file
+        //if it is needed for other options menu assets
+        mGame.getAssetManager().loadAssets("txt/assets/OptionScreenAssets.JSON");
+
+        mBackButton = new PushButton(
+                mDefaultLayerViewport.getWidth() * 0.88f, mDefaultLayerViewport.getHeight() * 0.10f,
+                mDefaultLayerViewport.getWidth() * 0.075f, mDefaultLayerViewport.getHeight() * 0.10f,
+                "BackArrow", "BackArrowSelected", this);
+        mButtons.add(mBackButton);
+
+        mOptionBackground = new GameObject(mDefaultLayerViewport.getWidth()/ 2.0f,
+                mDefaultLayerViewport.getHeight()/ 2.0f, mDefaultLayerViewport.getWidth(),
+                mDefaultLayerViewport.getHeight(), getGame()
+                .getAssetManager().getBitmap("OptionsBackground"), this);
     }
 
 
@@ -39,6 +63,18 @@ public class OptionsScreen extends GameScreen {
     public void update(ElapsedTime elapsedTime) {
         // Process any touch events occurring since the last update
         Input input = mGame.getInput();
+
+        List<TouchEvent> touchEvents = input.getTouchEvents();
+        if (touchEvents.size() > 0) {
+
+            // Update each button
+            for (PushButton button : mButtons)
+                button.update(elapsedTime);
+
+            if (mBackButton.isPushTriggered()) {
+                mGame.getScreenManager().removeScreen(this);
+            }
+        }
     }
 
     /**
@@ -50,5 +86,12 @@ public class OptionsScreen extends GameScreen {
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
         graphics2D.clear(Color.WHITE);
+
+        // Draw the background first of all
+        mOptionBackground.draw(elapsedTime, graphics2D, mDefaultLayerViewport,
+                mDefaultScreenViewport);
+
+        for (PushButton button : mButtons)
+            button.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
     }
 }
