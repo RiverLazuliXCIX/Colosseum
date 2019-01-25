7ef8b983c6dea1b2aba1e82e6bab3e04db18b509
Kyle Corrigan <kcorrigan06@qub.ac.uk>
2019-01-25 19:41:38 +0000

    Hero / Player functionality Added some further basic methods relating to the player class, such as additional getters and setters for private fields, the ability to increase the current mana, and current mana cap by 1 (for use upon the end of a turn), methods for increasing and decreasing attack, and a method handling the hero attacking with their weapon. Methods still need some further development to account for enemy heroes and minions etc.

--- app/src/main/java/uk/ac/qub/eeecs/game/Colosseum/Player.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/Colosseum/Player.java
@@ -15,8 +15,8 @@ public class Player extends GameObject {
     private static final float PORTRAIT_HEIGHT = 100.0f;
 
     // Sets a cap on the maximum health and mana a player can have at once
-    private final int maxHealth = 30;
-    private final int maxMana = 10;
+    private static final int maxHealth = 30;
+    private static final int maxMana = 10;
 
     private int currentHealth = maxHealth; // Current health at initialisation should be set to max.
     private int currentManaCap = 1;        // Initial mana cap should be set to one, and will increase by one each turn, until it reaches maxMana upon which it stops increasing (outside of potential card effects).
@@ -47,14 +47,26 @@ public class Player extends GameObject {
     // /////////////////////////////////////////////////////////////////////////
 
     /**
-     * Methods for retrieving the current health, armor and mana of the player.
+     * Getter and setter methods for each of the appropriate private variables
      */
 
     public int getCurrentHealth(){return currentHealth;}
+    public void setCurrentHealth(int currentHealth){this.currentHealth = currentHealth;}
+
+    public int getCurrentManaCap(){return currentManaCap;}
+    public void setCurrentManaCap(int currentManaCap){this.currentManaCap = currentManaCap;}
 
     public int getCurrentMana(){return currentMana;}
+    public void setCurrentMana(int currentMana){this.currentMana = currentMana;}
 
     public int getCurrentArmor(){return armor;}
+    public void setCurrentArmor(int armor){this.armor = armor;}
+
+    public int getCurrentAttack() {return attack;}
+    public void setCurrentAttack(int attack){this.attack = attack;}
+
+    public int getCurrentWeaponDurability() {return weaponDurability;}
+    public void setCurrentWeaponDurability(int weaponDurability) {this.weaponDurability = weaponDurability;}
 
     /**
      * Method for subtracting both health and armor when the player character/portrait
@@ -98,7 +110,7 @@ public class Player extends GameObject {
      *
      * @param manaReduction amount of mana attempting to be reduced.
      */
-    public void reduceMana(int manaReduction){
+    public void reduceCurrentMana(int manaReduction){
 
         // Currently, mana will only be reduced if the mana reduction can be covered by the player's
         // current mana, but until further card functionality is added (card costs, ability to play cards etc.)
@@ -111,13 +123,82 @@ public class Player extends GameObject {
     }
 
     /**
+     * Method to increase current mana. This function could be called based on specific card effects
+     * or abilities etc. (such as the coin card) Current implementation allows the mana to exceed
+     * the current mana cap, which would be common with the use of the coin card, allowing players to
+     * play more expensive cards earlier, or play more cards in a turn provided their current mana can
+     * cover the cost.
+     *
+     * @param manaAdded amount of mana attempting to be increased.
+     */
+    public void increaseCurrentMana(int manaAdded){
+        currentMana+=manaAdded;
+    }
+
+    /**
+     * Method for increasing the player character's current mana cap by 1 until the max mana cap has
+     * been reached. Method would be called when the turn ends.
+     *
+     */
+    public void increaseCurrentManaCap(){
+
+        if(currentManaCap < maxMana) {
+            currentManaCap += 1;
+        }
+
+    }
+
+    /**
      * Method for increasing the player character's armor. Armor is a secondary source of health
-     * and is reduced prior to the player's currentHealth.
+     * and is reduced prior to the player's currentHealth, serving as a method to somewhat increase
+     * the player's health cap.
      *
      * @param armorAdded amount of armor attempting to be added.
      */
-
     public void increaseArmor(int armorAdded){
         armor+=armorAdded;
+    }
+
+    /**
+     * Method for reducing the player character's attack. If the amount being reduced exceeds the
+     * current hero attack value, set attack to 0, to prevent negative attack values, else reduce
+     * the player attack by the amount expected.
+     *
+     * @param attackReduction amount of attack attempting to be reduced
+     */
+    public void reduceAttack(int attackReduction){
+
+        if(attackReduction>attack){
+            attack=0;
+        } else{
+            attack-=attackReduction;
+        }
+
+    }
+
+    /**
+     * Method for increasing the player character's attack. This could potentially be done through
+     * card abilities or weapons being equipped etc.
+     *
+     * @param attackAdded amount of attack being added to the player hero
+     */
+    public void increaseAttack(int attackAdded){
+        attack+=attackAdded;
+    }
+
+    /**
+     * Method for the player hero dealing weapon damage. Performs a check to ensure that the weapon
+     * durability is above 0, if true, enemy hero should receive damage and the hero's weapon
+     * durability should decrease by one.
+     * TODO Targeting enemy hero or minions when cards and enemy class has been implemented
+     * (current implementation only decreases durability)
+     *
+     */
+    public void dealWeaponDamage(){
+        // implement ability to target/interact with minions and heroes
+        if(weaponDurability >0) {
+            weaponDurability -= 1;
+        }
+        // Else if zero display message or some sort of feedback to the player
     }
 }
