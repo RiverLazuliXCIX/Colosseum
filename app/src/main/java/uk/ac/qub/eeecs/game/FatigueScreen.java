5bf099a9f466f0bb4ac78678c8daa36147b2e08d
duhbluh <dearbhailew@hotmail.com>
2019-02-26 18:13:22 +0000

    Completed User Story 2: Set up a 'FatigueScreen' that explains to the player that they've taken damage when they continue to draw from an empty deck

--- app/src/main/java/uk/ac/qub/eeecs/game/FatigueScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/FatigueScreen.java
@@ -1,6 +1,8 @@
 package uk.ac.qub.eeecs.game;
 
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 
 import java.util.List;
 
@@ -17,23 +19,48 @@ import uk.ac.qub.eeecs.game.Colosseum.colosseumDemoScreen;
 public class FatigueScreen extends GameScreen {
 
     //Variables required for the Fatigue Screen:
-    final private long FATIGUE_TIMEOUT = 3000;
-    private long timeOnCreate, currentTime;
-    private LayerViewport mFatigueScreenViewport;
-    private TitleImage ohNoText;
+    final private long FATIGUE_TIMEOUT = 5000;
+    private long mTimeOnCreate, mCurrentTime;
+    private long mTimeRemaining;
+    private int mDamageTaken = 0;
 
+    //Objects required for the appearance of the Fatigue Screen:
+    private LayerViewport mFatigueScreenViewport;
+    private TitleImage mOhNoText;
+    private Paint mText;
+    private Paint mSmallText;
 
     public FatigueScreen(Game game) {
         super("FatigueScreen", game);
-        timeOnCreate = System.currentTimeMillis();
+        mTimeOnCreate = System.currentTimeMillis();
         setupViewports();
         setUpFatigueScreenObjects();
     }
 
     private void setUpFatigueScreenObjects() {
-        //mGame.getAssetManager().loadAndAddBitmap("OhNo", "img/OhNo.png");
+        mGame.getAssetManager().loadAssets("txt/assets/FatigueScreenAssets.JSON");
 
-        //TitleImage ohNoText = new TitleImage(100, 100, 100, 50, "OhNo", this);
+        // Spacing that will be used to position the Fatigue Screen Objects:
+        int spacingX = (int) mDefaultLayerViewport.getWidth() / 5;
+        int spacingY = (int) mDefaultLayerViewport.getHeight() / 3;
+
+        // Create the title image
+        mOhNoText = new TitleImage(mDefaultLayerViewport.getWidth() / 2.0f, spacingY * 2.0f, spacingX*1.5f, spacingY/2.2f, "OhNoImg",this);
+
+        //PAINT OBJECT:
+        //Initialise Paint Objects I will use to draw text
+        mText = new Paint();
+        int screenHeight = mDefaultScreenViewport.height;
+        float textHeight = screenHeight / 20.0f;
+        mText.setTextSize(textHeight);
+        mText.setColor(Color.BLACK);
+        mText.setTypeface(Typeface.create("Arial", Typeface.BOLD));
+
+        mSmallText = new Paint();
+        float smallTextHeight = screenHeight / 24.0f;
+        mSmallText.setTextSize(smallTextHeight);
+        mSmallText.setColor(Color.BLACK);
+        mSmallText.setTypeface(Typeface.create("Arial", Typeface.BOLD));
     }
 
     private void setupViewports() {
@@ -53,24 +80,33 @@ public class FatigueScreen extends GameScreen {
         // Process any touch events occurring since the last update
         Input input = mGame.getInput();
 
+        mCurrentTime = System.currentTimeMillis();
+        mTimeRemaining = 5 - ((mCurrentTime - mTimeOnCreate)/1000);
+
         //Get current time and check for timeout
-        currentTime = System.currentTimeMillis();
-        if (currentTime - timeOnCreate >= FATIGUE_TIMEOUT) {
+        mCurrentTime = System.currentTimeMillis();
+        if (mCurrentTime - mTimeOnCreate >= FATIGUE_TIMEOUT) {
             mGame.getScreenManager().changeScreenButton(new colosseumDemoScreen(mGame));
         }
-
-        // Process any touch events occurring since the update
-        Input touchInput = mGame.getInput();
-        List<TouchEvent> touchEvents = touchInput.getTouchEvents();
-        if (touchEvents.size() > 0) {
-
-        }
-
     }
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
-        graphics2D.clear(Color.BLUE);
+        //Clear the screen
+        graphics2D.clear(Color.WHITE);
+
+        //Draw the title image
+        mOhNoText.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
+
+        // Spacing that will be used to position the Paint object:
+        float SCREEN_WIDTH = mGame.getScreenWidth();
+        float SCREEN_HEIGHT = mGame.getScreenWidth();
+
+        //Draw the text detailing fatigue function
+        graphics2D.drawText("You have run out of cards. You take " + mDamageTaken + " damage this turn.", SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.28f, mText);
+
+        //Draw the text showing the timer
+        graphics2D.drawText("Game will resume in " + mTimeRemaining + " seconds...", SCREEN_WIDTH * 0.46f, SCREEN_HEIGHT * 0.46f, mSmallText);
 
     }
 }
