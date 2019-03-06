353edb743c75c2937a15e7b7334a605996f99955
duhbluh <dearbhailew@hotmail.com>
2019-03-06 17:46:57 +0000

    Performed 3 unit tests on my FatigueScreen.

--- app/src/main/java/uk/ac/qub/eeecs/game/FatigueScreenForTesting.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/FatigueScreenForTesting.java
@@ -4,19 +4,16 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 
-import java.util.List;
-
 import uk.ac.qub.eeecs.gage.Game;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
 import uk.ac.qub.eeecs.gage.engine.input.Input;
-import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
 import uk.ac.qub.eeecs.gage.ui.TitleImage;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
 import uk.ac.qub.eeecs.gage.world.LayerViewport;
 import uk.ac.qub.eeecs.game.Colosseum.colosseumDemoScreen;
 
-public class FatigueScreen extends GameScreen {
+public class FatigueScreenForTesting extends GameScreen {
 
     //Variables required for the Fatigue Screen:
     final private long FATIGUE_TIMEOUT = 3000;
@@ -26,11 +23,9 @@ public class FatigueScreen extends GameScreen {
 
     //Objects required for the appearance of the Fatigue Screen:
     private LayerViewport mFatigueScreenViewport;
-    private TitleImage mOhNoText;
-    private Paint mText;
-    private Paint mTimerText;
 
-    public FatigueScreen(Game game, int damageToTake) {
+
+    public FatigueScreenForTesting(Game game, int damageToTake) {
         super("FatigueScreen", game);
         mTimeOnCreate = System.currentTimeMillis();
         this.mDamageTaken = damageToTake;
@@ -39,30 +34,9 @@ public class FatigueScreen extends GameScreen {
         setUpFatigueScreenObjects();
     }
 
+    //Methods
     private void setUpFatigueScreenObjects() {
-        mGame.getAssetManager().loadAssets("txt/assets/FatigueScreenAssets.JSON");
 
-        // Spacing that will be used to position the Fatigue Screen Objects:
-        int spacingX = (int) mDefaultLayerViewport.getWidth() / 5;
-        int spacingY = (int) mDefaultLayerViewport.getHeight() / 3;
-
-        // Create the title image
-        mOhNoText = new TitleImage(mDefaultLayerViewport.getWidth() / 2.0f, spacingY * 2.0f, spacingX*1.5f, spacingY/2.2f, "OhNoImg",this);
-
-        //PAINT OBJECT:
-        //Initialise Paint Objects I will use to draw text
-        mText = new Paint();
-        int screenHeight = mDefaultScreenViewport.height;
-        float textHeight = screenHeight / 20.0f;
-        mText.setTextSize(textHeight);
-        mText.setColor(Color.BLACK);
-        mText.setTypeface(Typeface.create("Arial", Typeface.BOLD));
-
-        mTimerText = new Paint();
-        float smallTextHeight = screenHeight / 24.0f;
-        mTimerText.setTextSize(smallTextHeight);
-        mTimerText.setColor(Color.BLACK);
-        mTimerText.setTypeface(Typeface.create("Arial", Typeface.BOLD));
     }
 
     private void setupViewports() {
@@ -95,24 +69,10 @@ public class FatigueScreen extends GameScreen {
 
     @Override
     public void draw(ElapsedTime elapsedTime, IGraphics2D graphics2D) {
-        //Clear the screen
-        graphics2D.clear(Color.WHITE);
-
-        //Draw the title image
-        mOhNoText.draw(elapsedTime, graphics2D, mDefaultLayerViewport, mDefaultScreenViewport);
-
-        // Spacing that will be used to position the Paint object:
-        float SCREEN_WIDTH = mGame.getScreenWidth();
-        float SCREEN_HEIGHT = mGame.getScreenWidth();
-
-        //Draw the text detailing fatigue function
-        graphics2D.drawText("You have run out of cards. You take " + mDamageTaken + " damage this turn.", SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.28f, mText);
-
-        //Draw the text showing the timer
-        graphics2D.drawText("Game will resume in " + mTimeRemaining + " seconds...", SCREEN_WIDTH * 0.46f, SCREEN_HEIGHT * 0.46f, mTimerText);
 
     }
 
     //Getters and setters:
     public int getmDamageTaken() { return this.mDamageTaken; }
+
 }
---
+++ app/src/test/java/uk/ac/qub/eeecs/gage/FatigueScreenTest.java
@@ -0,0 +1,58 @@
+package uk.ac.qub.eeecs.gage;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import uk.ac.qub.eeecs.game.FatigueScreenForTesting;
+
+import static org.junit.Assume.assumeNotNull;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FatigueScreenTest {
+
+    @Mock
+    private FatigueScreenForTesting mFatigueScreen;
+    @Mock
+    private Game mGame;
+
+    @Before
+    public void setUp() {
+        when(mFatigueScreen.getGame()).thenReturn(mGame);
+        when(mFatigueScreen.getName()).thenReturn("FatigueScreen");
+    }
+
+    //The following tests are to test the FatigueScreen constructor:
+    @Test
+    public void constructor_SetUpFully() {
+        //This test is to ensure that the constructor successfully creates an instance of FatigueScreen
+        FatigueScreenForTesting f1 = new FatigueScreenForTesting(mGame, 5);
+
+        assumeNotNull(f1);
+    }
+
+    @Test
+    public void constructor_NameCorrect() {
+        //This test is to ensure that the correct name is assigned to the new FatigueScreen
+        FatigueScreenForTesting f1 = new FatigueScreenForTesting(mGame, 5);
+
+        String expectedResult = "FatigueScreen";
+
+        assertEquals(expectedResult, f1.getName());
+    }
+
+    @Test
+    public void damageTakenSetCorrectly() {
+        //This test is to ensure that whatever damage is passed into the class, is assigned to
+        //the mDamageTaken variable within the FatigueScreen class
+        FatigueScreenForTesting f1 = new FatigueScreenForTesting(mGame, 5);
+
+        int expectedResult = 5;
+
+        assertEquals(expectedResult, f1.getmDamageTaken());
+    }
+}
