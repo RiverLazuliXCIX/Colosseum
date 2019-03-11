67c53f4fb60560d987e41eb661b8516c94e6272f
duhbluh <dearbhailew@hotmail.com>
2019-03-11 17:37:19 +0000

    User Story 27: Refactor 'CardDeck' class so that coin costs/damage/health etc are variable each time you draw a card, instead of being stuck at same value. Still needs further refactoring to remove redundancy.

--- app/src/main/java/uk/ac/qub/eeecs/game/Colosseum/CardDeck.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/Colosseum/CardDeck.java
@@ -11,34 +11,25 @@ public class CardDeck {
     //Variables for Card Deck
     private int mDeckID;
     private String mDeckName;
-    private int mNumOfCards = 0;
-    private int mSizeOfHand = 0;
-    private int mSizeOfDiscard = 0;
+    private int mNumOfCards = 0, mSizeOfHand = 0, mSizeOfDiscard = 0;
     private GameScreen mGameScreen;
 
     //ArrayList to hold the deck of cards, and the card graveyard
-    private ArrayList<Card> mDeck;
-    private ArrayList<Card> mDiscardPile;
-    private ArrayList<Card> mCardHand;
+    private ArrayList<Card> mDeck, mDiscardPile, mCardHand;
 
     //Boolean values required to discern the functionality of the deck:
-    private boolean mIsEmptyFlag;
-    private boolean mIsAIDeck = false;
+    private boolean mIsEmptyFlag, mIsAIDeck = false;
 
     //Constant values relating to Card Deck and Hand
-    public static final int MAX_DECK_CARDS = 30;
-    public static final int MAX_HAND_CARDS = 5;
+    public static final int MAX_DECK_CARDS = 30, MAX_HAND_CARDS = 5;
 
     //Random, to be used to 'choose' type of deck:
     private static final Random RANDOM = new Random();
 
-    private static final float X_POSITION = 160f;
-    private static final float Y_POSITION = 25f;
+    private static final float X_POSITION = 160f, Y_POSITION = 25f;
 
     //Integer variables to be used to determine deck 'type':
-    int mNumOfMinions;
-    int mNumOfSpells;
-    int mNumOfWeapons;
+    private int mNumOfMinions, mNumOfSpells, mNumOfWeapons;
 
     // Corresponds to the associated hand region where cards will be drawn to
     private HandRegion mHandRegion;
@@ -69,82 +60,107 @@ public class CardDeck {
         this.buildDeck();
     }
 
-     /**
-     /Public methods required for CardDeck class:
-     **/
-
+     //Public methods required for CardDeck class:
     public void buildDeck() {
         chooseDeckType();
-        addToCardNum();
         insertMinionCard(mNumOfMinions);
         insertSpellCard(mNumOfSpells);
         insertWeaponCard(mNumOfWeapons);
     }
 
+    public void setUpDeckOptions(int mMinNum, int mSpellNum, int mWeapNum) {
+        mNumOfMinions = mMinNum;
+        mNumOfCards += mNumOfMinions;
+        mNumOfSpells = mSpellNum;
+        mNumOfCards += mNumOfSpells;
+        mNumOfWeapons = mWeapNum;
+        mNumOfCards += mNumOfWeapons;
+    }
+
     public void chooseDeckType() {
         int mType = RANDOM.nextInt(3);
-
         switch (mType) {
             case 0:
-                mNumOfMinions = 10;
-                mNumOfSpells = 10;
-                mNumOfWeapons = 10;
+                setUpDeckOptions(10, 10, 10);
                 break;
             case 1:
-                mNumOfMinions = 15;
-                mNumOfSpells = 10;
-                mNumOfWeapons = 5;
+                setUpDeckOptions(15, 10, 5);
                 break;
             case 2:
-                mNumOfMinions = 10;
-                mNumOfSpells = 15;
-                mNumOfWeapons = 5;
+                setUpDeckOptions(10, 15, 5);
                 break;
         }
     }
 
-    // Methods to add different types of cards to the deck:
-    public void addToCardNum() {
-        mNumOfCards += mNumOfMinions;
-        mNumOfCards += mNumOfWeapons;
-        mNumOfCards += mNumOfSpells;
-    }
-
-
-    //TODO: change x and y pos to use screen widths
+    //TODO: change x and y pos to use screen widths - Sean
     public void insertMinionCard(int cardAmt) {
         for (int i = 0; i < cardAmt; i++) {
             float x = X_POSITION;
             float y = Y_POSITION;
             Boolean enemyDeck = false;
-            if(getDeckID() == 2) {
+            if (getDeckID() == 2) {
                 enemyDeck = true;
                 y *= 9.8f;
             }
             //x += 33.4f * i;
             Random rand = new Random();
             String name = "";
+            int coinCost = 0;
+            int attack = 0;
+            int health = 0;
+
             int no = rand.nextInt(7);
-            if (no == 0)
-                name = "Card_Archer";
-            else if (no == 1)
-                name = "Card_Centurion";
-            else if (no == 2)
-                name = "Card_Cerberus";
-            else if (no == 3)
-                name = "Card_Lion";
-            else if (no == 4)
-                name = "Card_Veles";
-            else if (no == 5)
-                name = "Card_Veteran";
-            else if (no == 6)
-                name = "Card_Elephant";
-            else if (no == 7)
-                name = "Card_Hound";
+            switch (no) {
+                case 0:
+                    name = "Card_Archer";
+                    coinCost = 2;
+                    attack = 3;
+                    health = 2;
+                    break;
+                case 1:
+                    name = "Card_Centurion";
+                    coinCost = 3;
+                    attack = 2;
+                    health = 2;
+                    break;
+                case 2:
+                    name = "Card_Cerberus";
+                    coinCost = 1;
+                    attack = 2;
+                    health = 1;
+                    break;
+                case 3:
+                    name = "Card_Lion";
+                    coinCost = 6;
+                    attack = 5;
+                    health = 4;
+                    break;
+                case 4:
+                    name = "Card_Veles";
+                    coinCost = 4;
+                    attack = 3;
+                    health = 4;
+                    break;
+                case 5:
+                    name = "Card_Veteran";
+                    coinCost = 2;
+                    attack = 1;
+                    health = 3;
+                    break;
+                case 6:
+                    name = "Card_Elephant";
+                    coinCost = 9;
+                    attack = 3;
+                    health = 8;
+                case 7:
+                    name = "Card_Hound";
+                    coinCost = 1;
+                    attack = 1;
+                    health = 4;
+            }
 
-            MinionCard mTestMinion = new MinionCard(x, y, mGameScreen, 3, enemyDeck, name, 2, 1);
-
-            mDeck.add(i, mTestMinion);
+            MinionCard mMinion = new MinionCard(x, y, mGameScreen, coinCost, enemyDeck, name, attack, health);
+            mDeck.add(i, mMinion);
         }
     }
 
@@ -160,29 +176,71 @@ public class CardDeck {
             //x += 33.4 * i;
             Random rand = new Random();
             String name = "";
+            int coinCost = 0;
+            Effect effect;
+            int magnitude = 0;
             int no = rand.nextInt(8);
-            if (no == 0)
-                name = "Card_Aegis";
-            else if (no == 1)
-                name = "Card_Battleshout";
-            else if (no == 2)
-                name = "Card_Cavalry";
-            else if (no == 3)
-                name = "Card_Commander";
-            else if (no == 4)
-                name = "Card_Touch";
-            else if (no == 5)
-                name = "Card_Aurora";
-            else if (no == 6)
-                name = "Card_LionWarCry";
-            else if (no == 7)
-                name = "Card_Strike";
-            else if (no == 8)
-                name = "Card_Rete";
 
-            SpellCard mTestSpell = new SpellCard(x, y, mGameScreen, 3, enemyDeck, name, Effect.NONE, 1);
+            switch (no) {
+                case 0:
+                    name = "Card_Aegis";
+                    coinCost = 3;
+                    effect = Effect.NONE;
+                    magnitude = 3;
+                    break;
+                case 1:
+                    name = "Card_Battleshout";
+                    coinCost = 5;
+                    effect = Effect.NONE;
+                    magnitude = 2;
+                    break;
+                case 2:
+                    name = "Card_Cavalry";
+                    coinCost = 3;
+                    effect = Effect.NONE;
+                    magnitude = 2;
+                    break;
+                case 3:
+                    name = "Card_Commander";
+                    coinCost = 6;
+                    effect = Effect.NONE;
+                    magnitude = 4;
+                    break;
+                case 4:
+                    name = "Card_Touch";
+                    coinCost = 5;
+                    effect = Effect.NONE;
+                    magnitude = 4;
+                    break;
+                case 5:
+                    name = "Card_Aurora";
+                    coinCost = 2;
+                    effect = Effect.NONE;
+                    magnitude = 1;
+                    break;
+                case 6:
+                    name = "Card_LionWarCry";
+                    coinCost = 4;
+                    effect = Effect.NONE;
+                    magnitude = 3;
+                    break;
+                case 7:
+                    name = "Card_Strike";
+                    coinCost = 4;
+                    effect = Effect.NONE;
+                    magnitude = 2;
+                    break;
+                case 8:
+                    name = "Card_Rete";
+                    coinCost = 6;
+                    effect = Effect.NONE;
+                    magnitude = 5;
+                    break;
+            }
 
-            mDeck.add(i, mTestSpell);
+            SpellCard mSpell = new SpellCard(x, y, mGameScreen, 3, enemyDeck, name, Effect.NONE, 1);
+
+            mDeck.add(i, mSpell);
         }
     }
 
@@ -191,28 +249,45 @@ public class CardDeck {
             float x = X_POSITION;
             float y = Y_POSITION;
             Boolean enemyDeck = false;
-            if(getDeckID() == 2) {
+            if (getDeckID() == 2) {
                 enemyDeck = true;
                 y *= 9.8f;
             }
             //x += 33.4f * i;
             Random rand = new Random();
             String name = "";
+            int coinCost = 0, damage = 0, charges = 0;
             int no = rand.nextInt(3);
-            if (no == 0)
-                name = "Card_Hasta";
-            else if (no == 1)
-                name = "Card_Axe";
-            else if (no == 2)
-                name = "Card_Scourge";
-            else if (no == 3)
-                name = "Card_Bow";
-
-            WeaponCard mTestWeapon = new WeaponCard(x, y, mGameScreen, 3, enemyDeck, name, 2, 1);
-
-            mDeck.add(i, mTestWeapon);
+            switch (no) {
+                case 0:
+                    name = "Card_Hasta";
+                    coinCost = 4;
+                    damage = 3;
+                    charges = 2;
+                    break;
+                case 1:
+                    name = "Card_Axe";
+                    coinCost = 5;
+                    damage = 4;
+                    charges = 3;
+                    break;
+                case 2:
+                    name = "Card_Scourge";
+                    coinCost = 5;
+                    damage = 5;
+                    charges = 1;
+                    break;
+                case 3:
+                    name = "Card_Bow";
+                    coinCost = 3;
+                    damage = 2;
+                    charges = 3;
+                    break;
+            }
+                WeaponCard mWeapon = new WeaponCard(x, y, mGameScreen, coinCost, enemyDeck, name, damage, charges);
+                mDeck.add(i, mWeapon);
+            }
         }
-    }
 
     //Methods required to draw from Deck:
     public Card drawTopCard() {
@@ -227,15 +302,6 @@ public class CardDeck {
         }
         return null;
     }
-
-    private void trackAdditionOfCardsToHand () {
-        mSizeOfHand ++;
-    }
-
-    private void trackRemovalOfCards() {
-        mNumOfCards --;
-    }
-
     //This method destroys a card if player draws one, when their hand is already full:
     public void destroyCardOverLimit() {
         if (mCardHand.size() > MAX_HAND_CARDS) {
@@ -252,6 +318,14 @@ public class CardDeck {
             setmIsEmptyFlag(true);
         }
         return mIsEmptyFlag;
+    }
+
+    private void trackAdditionOfCardsToHand () {
+        mSizeOfHand ++;
+    }
+
+    private void trackRemovalOfCards() {
+        mNumOfCards --;
     }
 
     //Accessor and Mutator methods:
